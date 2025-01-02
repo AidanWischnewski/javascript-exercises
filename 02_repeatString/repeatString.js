d0b8d3e2e40182a1a59bc18e08390fb114b0bacf
Aidan <aidan.wischnewski@gmail.com>
2025-01-01 16:46:37 -0800

    exercose 1 and 2 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, repititions) {
+    if (repititions < 0){
+        return "ERROR"
+    }
+    result = "";
+    for ( i=0; i < repititions; i++){
+        result += string
+    }
+    return result
 };
 
+repeatString("hey",2)
 // Do not edit below this line
 module.exports = repeatString;
